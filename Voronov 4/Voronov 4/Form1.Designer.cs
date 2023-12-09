f65a1f47880e1eec1de4a1509c2b2dc30fb57265
M1lc3 <sofarua@gmail.com>
2023-12-10 00:28:14 +0200

    v0.5

---
+++ Voronov 4/Voronov 4/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Voronov_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1030, 562);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(1030, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Багатопоточність";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0 ms";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(0, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(922, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Побудувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Voronov 4/Voronov 4/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace Voronov_4
+{
+    public partial class Form1 : Form
+    {
+        private Point[] points = new Point[0];
+        private Random random = new Random();
+        private Color[] colors = new Color[0];
+        static Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        Graphics g = Graphics.FromImage(bitmap);
+        private bool multithreadingEnabled = false;
+        Stopwatch stopwatch = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();    
+        }
+
+        private void CreatePoints()
+        {
+            int count = (int)numericUpDown1.Value;
+            points = new Point[count];
+            colors = new Color[count];
+            for (int i = 0; i < count; i++)
+            {
+                points[i] = new Point(random.Next(0, pictureBox1.Width), random.Next(0, pictureBox1.Height));
+                colors[i] = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+            }
+        }
+
+        private void DrawVoronoi(object threadRange)
+        {
+            int[] range = (int[])threadRange;
+            int startY = range[0];
+            int endY = range[1];
+
+            for (int x = 0; x < this.ClientSize.Width; x++)
+            {
+                for (int y = startY; y < endY; y++)
+                {
+                    int minDistance = int.MaxValue;
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        int distance = (x - points[i].X) * (x - points[i].X) + (y - points[i].Y) * (y - points[i].Y);
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            lock (g)
+                            {
+                                g.FillRectangle(new SolidBrush(colors[i]), x, y, 1, 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void DrawVoronoiOneThread()
+        {
+            stopwatch.Restart();
+            stopwatch.Start();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            g.Clear(Color.White);
+            CreatePoints();
+            if (points != null && points.Length > 0)
+            {
+
+                for (int x = 0; x < this.ClientSize.Width; x++)
+                {
+                    for (int y = 0; y < this.ClientSize.Height; y++)
+                    {
+                        int minDistance = int.MaxValue;
+                        for (int i = 0; i < points.Length; i++)
+                        {
+                            int distance = (x - points[i].X) * (x - points[i].X) + (y - points[i].Y) * (y - points[i].Y);
+                            if (distance < minDistance)
+                            {
+                                minDistance = distance;
+                                g.FillRectangle(new SolidBrush(colors[i]), x, y, 1, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (Point p in points)
+            {
+                g.FillEllipse(Brushes.Black, p.X - 3, p.Y - 3, 6, 6);
+            }
+            pictureBox1.Image = bitmap;
+            stopwatch.Stop();
+            label1.Text = timeSpan.TotalMilliseconds.ToString() + " mc";
+        }
+        private void DrawVoronoiMultiThreads()
+        {
+            stopwatch.Restart();
+            stopwatch.Start();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            g.Clear(Color.White);
+            CreatePoints();
+            if (points != null && points.Length > 0)
+            {
+                int numThreads = Environment.ProcessorCount;
+                int sliceHeight = this.ClientSize.Height / numThreads;
+                Thread[] threads = new Thread[numThreads];
+
+                for (int i = 0; i < numThreads; i++)
+                {
+                    int startY = i * sliceHeight;
+                    int endY = (i + 1) * sliceHeight;
+                    if (i == numThreads - 1) // For the last thread, process the remaining rows
+                    {
+                        endY = this.ClientSize.Height;
+                    }
+                    int[] range = new int[] { startY, endY };
+                    threads[i] = new Thread(new ParameterizedThreadStart(DrawVoronoi));
+                    threads[i].Start(range);
+                }
+                for (int i = 0; i < numThreads; i++)
+                {
+                    threads[i].Join(); // Wait for all threads to finish
+                }
+            }
+            foreach (Point p in points)
+            {
+                g.FillEllipse(Brushes.Black, p.X - 3, p.Y - 3, 6, 6);
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            if (multithreadingEnabled) DrawVoronoiMultiThreads();
+            else DrawVoronoiOneThread();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            stopwatch.Stop();
+            label1.Text = timeSpan.TotalMilliseconds.ToString() + " mc";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            multithreadingEnabled = checkBox1.Checked;
+        }
+    }
+}
